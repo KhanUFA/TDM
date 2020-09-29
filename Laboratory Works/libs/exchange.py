46bc4540f2b44ac981727685d657eb0d1f4551e5
KhanUFA <lymon700@gmail.com>
2020-09-29 22:14:36 +0500

    Update exchange.py

--- Laboratory Works/libs/exchange.py
+++ Laboratory Works/libs/exchange.py
@@ -68,3 +68,21 @@ class Rate:
     def AZN(self):
         """Возвращает курс азербайджанского маната на сегодня в формате self.format"""
         return self.make_format('AZN')
+
+class Employee:
+    def __init__(self, name, seniority):
+        self.name = name
+        self.seniority = seniority
+        
+        self.grade = 1
+    
+    def grade_up(self):
+        """Повышает уровень сотрудника"""
+        self.grade += 1
+    
+    def publish_grade(self):
+        """Публикация результатов аккредитации сотрудников"""
+        print(self.name, self.grade)
+    
+    def check_if_it_is_time_for_upgrade(self):
+        pass
